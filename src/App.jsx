05444631e2a3ef4fdcc82afc47d903fca6a53aca
muqaddasazahra <muqaddasazahra@gmail.com>
2024-02-26 13:37:16 +0500

    ProjectState added to switch between newProject and noProject Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectSideBar";
+import { ContainerWithChildren } from "postcss/lib/container";
 
 function App() {
+  const [projectState,setProjectState]=useState({
+    selectedProjectId:undefined,
+    projects:[]
+})
+function handleAddProject()
+{
+  setProjectState(prevState=>
+    {
+      return {
+        ...prevState,
+        selectedProjectId:null
+      }
+
+    })
+}
+
+let content;
+if(projectState.selectedProjectId===null)
+content=<NewProject/>
+else if(projectState.selectedProjectId===undefined)
+content= <NoProjectSelected onAddProject={handleAddProject}/>
+
   return (
     
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar/>
-      <NoProjectSelected/>
+      <ProjectSideBar onAddProject={handleAddProject}/>
+      {content}
     </main>
     
     
